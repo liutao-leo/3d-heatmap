028b01a425f2c1c0d75f05e706597475213a3dc4
Zhang Li <xiaoxin88214@163.com>
2021-11-09 16:09:49 +0800

    floor build

--- src/helper.js
+++ src/helper.js
@@ -8,9 +8,9 @@ axiosInstance.interceptors.request.use(
   config => {
     config.headers['Content-Type'] = 'application/json; charset=UTF-8'
     config.headers['user-token'] =
-      'eyJhbGciOiJIUzUxMiIsImlhdCI6MTYzNjE4NjA4NCwiZXhwIjoxNjM3NDgyMDg0fQ.eyJpZCI6MTF9.X3oRpZIRmFzIMf2ChhRwd2WV7Foi7vcl5Ycfk3l46DIYphP7r7sssMCnWFqcuQ9oxUS9K_JpIo7Tiy5Vnru9JQ'
+      'eyJhbGciOiJIUzUxMiIsImlhdCI6MTYzNjM4MjYwNywiZXhwIjoxNjM3Njc4NjA3fQ.eyJpZCI6MTJ9.PVD4nDgup80AdQ10Y2UWE6OTn6gf3SiOZztcev-NOqlTl0RpidjgXif8t1-RLnwU7e2I7xmZ_JECrk4k6RUdLQ'
     config.headers['mall-token'] =
-      'eyJhbGciOiJIUzUxMiJ9.eyJpZCI6MTF9.-jRLdLOEefWTmXxs1Re3ZXXGH2tmgF8AigVrEkedXeBo5TrfN5LezTfyz965WctXVLsKDdsWjt7lM0qk9vugHg'
+      'eyJhbGciOiJIUzUxMiJ9.eyJpZCI6MTJ9.G7r0gp31YTywyHukrh8bN8pMfuyEtzgiPQCefqo7rDdICr0YvDg8Cbgi-W1zF0yg4h9LkUNFS5fy1g0GQNfd5w'
     // 删除headers中key值为空(falsy)的header
     for (const prop in config.headers) {
       if (!config.headers[prop]) {
