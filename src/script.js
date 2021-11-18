9669f28ae53326b1b5d0dc805af317c9cfcf0418
leo <liutao.leo@outlook.com>
2021-11-18 16:41:47 +0800

    feat: 添加3d热力图

--- src/script.js
+++ src/script.js
@@ -5,6 +5,8 @@ import { Earcut } from 'three/src/extras/Earcut'
 import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js'
 import * as dat from 'dat.gui'
 import axiosInstance from './helper'
+import floorData from './floor.json'
+import heatmapInstance from './heatmap'
 
 const buildColors = [0x219fd4, 0x24aee8, 0x4ec1f1]
 const getGeometry = (points, height) => {
@@ -81,31 +83,153 @@ export const drawBuild = points => {
   return mesh
 }
 
-axiosInstance.get('/floor').then(response => {
-  console.log('response')
-  console.log('floor data', response.data)
-  // B1层楼
-  const coordinates = response.data[0].coordinate
-  console.log('coordinates', coordinates)
+//绘制热力图
+export const drawHeatMap = heatCanvas => {
+  // console.log(heatCanvas)
+  // const width = heatCanvas.width
+  // const height = heatCanvas.height
+  const width = 1200
+  const height = 800
+  const texture = new THREE.Texture(heatCanvas)
+  const material = new THREE.MeshBasicMaterial({
+    map: texture,
+    transparent: true,
+    depthTest: false,
+    depthWrite: false,
+    side: THREE.DoubleSide,
+  })
+  material.map.needsUpdate = true
+  const mesh = new THREE.Mesh(new THREE.PlaneGeometry(width, height), material)
+  mesh.rotation.x = -Math.PI / 2
+  return mesh
+}
+
+// axiosInstance.get('/floor').then(response => {
+//   console.log('response')
+//   console.log('floor data', response.data)
+//   console.log(document.getElementById('heatmap'))
+//   // F1层楼
+//   const coordinates = response.data[3].coordinate
+//   console.log('coordinates', coordinates)
+
+//   const heatmap = document.getElementById('heatmap')
+
+//   //   console.log('---sub----', THREE.Face3)
+//   const width = heatmap.clientWidth
+//   const height = heatmap.clientHeight
+//   // const width = window.innerWidth
+//   // const height = window.innerHeight
+//   const scene = new THREE.Scene()
+//   // 设置摄像机位置，并将其朝向场景
+//   const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000)
+//   //设置三维坐标
+//   camera.position.set(100, 100, 100)
+//   //设置相机看上边的坐标
+//   // camera.lookAt(scene.position)
+//   camera.lookAt(0, 0, 0)
+
+//   const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
+//   renderer.setPixelRatio(window.devicePixelRatio)
+//   renderer.setClearColor(0x000000, 0)
+//   renderer.setSize(width, height)
+//   heatmap.appendChild(renderer.domElement)
+
+//   // 场景控制器
+//   const orbit = new OrbitControls(camera, renderer.domElement)
+//   // orbit.enableRotate = false
+//   //设置相机的角度范围
+//   orbit.maxPolarAngle = Math.PI / 2
+//   //设置相机距离原点的距离
+//   orbit.maxDistance = 1000
+//   orbit.minDistance = 400
+//   // 设置控制器垂直旋转的角度
+//   orbit.maxPolarAngle = Math.PI * 2
+//   orbit.minPolarAngle = -Math.PI * 2
+//   orbit.mouseButtons = {
+//     LEFT: THREE.MOUSE.PAN,
+//     MIDDLE: THREE.MOUSE.DOLLY,
+//     RIGHT: THREE.MOUSE.ROTATE,
+//   }
+//   orbit.update()
+
+//   scene.add(new THREE.AxesHelper(10))
+//   const floorGroup = new THREE.Group()
+
+//   // 物体转换控制器
+//   const trans = new TransformControls(camera, renderer.domElement)
+//   trans.setMode('translate')
+//   // 默认关闭Y轴 (rotate才开启). scale translate 不允许Y轴上的变动
+//   // trans.showY = false
+//   scene.add(trans)
+
+//   // 画楼层和建筑
+//   const scale = 8
+//   // const floorCordinates = coordinates[0].map(p => [
+//   //   p[0] / scale,
+//   //   0,
+//   //   p[1] / scale,
+//   // ])
+//   // console.log('floor cordinates', floorCordinates)
+
+//   // floorGroup.add(drawFloor(1, floorCordinates))
+
+//   const end = coordinates.length
+
+//   for (let i = 0; i < end; i++) {
+//     const points = coordinates[i]
+//     console.log(`coordinates ${i}`, points)
+//     const g = new THREE.Group()
+//     floorGroup.add(g)
+//     g.add(drawBuild(points.map(p => [p[0] / scale, 0, p[1] / scale])))
+//   }
+
+//   scene.add(floorGroup)
+
+//   // renderer.render(scene, camera)
+//   const animate = () => {
+//     requestAnimationFrame(animate)
+
+//     // required if controls.enableDamping or controls.autoRotate are set to true
+//     orbit.update()
+
+//     renderer.render(scene, camera)
+//   }
+//   animate()
+// })
+
+function draw() {
+  // console.log('response')
+  // console.log('floor data', response.data)
+  // console.log(document.getElementById('heatmap'))
+  // console.log(floorData)
+  // F1层楼
+  // const coordinates = response.data[3].coordinate
+  const coordinates = floorData[3].coordinate
+  // console.log('coordinates', coordinates)
+
+  const heatmap = document.getElementById('heatmap')
 
   //   console.log('---sub----', THREE.Face3)
-  const width = window.innerWidth
-  const height = window.innerHeight
+  // const width = heatmap.clientWidth
+  // const height = heatmap.clientHeight
+  const width = 1200
+  const height = 800
+  // const width = window.innerWidth
+  // const height = window.innerHeight
   const scene = new THREE.Scene()
-
   // 设置摄像机位置，并将其朝向场景
-  const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000)
+  const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000)
   //设置三维坐标
-  camera.position.set(100, 100, 100)
+  camera.position.set(0, 100, 100)
   //设置相机看上边的坐标
-  // camera.lookAt(scene.position)
-  camera.lookAt(0, 0, 0)
+  camera.lookAt(scene.position)
+  // camera.lookAt(0, 0, 0)
 
   const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true })
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setClearColor(0x000000, 0)
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  document.body.appendChild(renderer.domElement)
+  renderer.setSize(width, height)
+  heatmap.appendChild(renderer.domElement)
 
   // 场景控制器
   const orbit = new OrbitControls(camera, renderer.domElement)
@@ -113,8 +237,8 @@ axiosInstance.get('/floor').then(response => {
   //设置相机的角度范围
   orbit.maxPolarAngle = Math.PI / 2
   //设置相机距离原点的距离
-  orbit.maxDistance = 2000
-  orbit.minDistance = 300
+  orbit.maxDistance = 1500
+  orbit.minDistance = 400
   // 设置控制器垂直旋转的角度
   orbit.maxPolarAngle = Math.PI * 2
   orbit.minPolarAngle = -Math.PI * 2
@@ -137,25 +261,25 @@ axiosInstance.get('/floor').then(response => {
 
   // 画楼层和建筑
   const scale = 8
-  const floorCordinates = coordinates[0].map(p => [
-    p[0] / scale,
-    0,
-    p[1] / scale,
-  ])
-  console.log('floor cordinates', floorCordinates)
+  // const floorCordinates = coordinates[0].map(p => [
+  //   p[0] / scale,
+  //   0,
+  //   p[1] / scale,
+  // ])
+  // console.log('floor cordinates', floorCordinates)
 
-  floorGroup.add(drawFloor(1, floorCordinates))
+  // floorGroup.add(drawFloor(1, floorCordinates))
 
   const end = coordinates.length
-
-  for (let i = 1; i < end; i++) {
+  const g = new THREE.Group()
+  for (let i = 0; i < end; i++) {
     const points = coordinates[i]
-    console.log(`coordinates ${i}`, points)
-    const g = new THREE.Group()
-    floorGroup.add(g)
+    // console.log(`coordinates ${i}`, points)
     g.add(drawBuild(points.map(p => [p[0] / scale, 0, p[1] / scale])))
   }
 
+  g.add(drawHeatMap(heatmapInstance._renderer.canvas))
+  floorGroup.add(g)
   scene.add(floorGroup)
 
   // renderer.render(scene, camera)
@@ -168,4 +292,6 @@ axiosInstance.get('/floor').then(response => {
     renderer.render(scene, camera)
   }
   animate()
-})
+}
+
+draw()
